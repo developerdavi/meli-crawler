b05e39d75cb035625ed8309d79741a5d9966c860
Davi Rodrigues <developer.davi@gmail.com>
2020-02-12 12:22:55 -0300

    Created "/" (index) route to display some info

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <main>
+      <h1>Welcome to Meli Crawler!</h1>
+      <p>Look at the docs: <a href="https://github.com/developerdavi/meli-crawler.git">Github Repo</a> </p>
+    </main>
+  )
+}
