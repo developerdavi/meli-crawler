f1fb084a8d53caf4e42fb4a88e4e74824387864e
Davi Rodrigues <developer.davi@gmail.com>
2020-02-12 18:56:16 -0300

    Added Cache-Control header

--- pages/api/sellers/[id].js
+++ pages/api/sellers/[id].js
@@ -4,6 +4,9 @@ const { MELI_APP, MELI_SECRET } = process.env
 
 export default function Seller(request, response) {
   const { id } = request.query
+
+  // CACHING
+  response.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
   
   const meli = new mercadoLibre.Meli(MELI_APP, MELI_SECRET)
 
