cce4bd14dc30bfcabc1c78862c4eac40e5f1b131
Davi Rodrigues <developer.davi@gmail.com>
2020-02-12 13:32:14 -0300

    Changed Cache-Control header

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -6,13 +6,15 @@ export default async function Products(request, response) {
   const { body } = request
 
   // CACHING
-  response.setHeader('Cache-Control', 'max-age=0, s-maxage=86400')
+  response.setHeader('Cache-Control', 'max-age=3600, s-maxage=86400')
 
   try {
     const meli = new mercadoLibre.Meli(MELI_APP, MELI_SECRET)
   
     meli.get(`/sites/MLB/search?q=${body.search}`, null, (err, res) => {
       
+      response.status(200)
+
       // SEND PRODUCTS RESPONSE
       response.json(res.results)
     })
