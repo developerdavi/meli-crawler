2c18491724956158d170f88ad06241829e0e47c1
Davi Rodrigues <developer.davi@gmail.com>
2020-02-12 18:08:47 -0300

    Changed Cache-Control

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -11,7 +11,7 @@ export default async function Products(request, response) {
   const limit = body.limit || 20
 
   // CACHING
-  response.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
+  response.setHeader('Cache-Control', 'max-age=0, s-maxage=86400')
 
   try {
     const meli = new mercadoLibre.Meli(MELI_APP, MELI_SECRET)
