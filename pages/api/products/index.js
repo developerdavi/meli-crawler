d5905f2b74191c3e6ae1aa7964255e9f2c4c5595
Davi Rodrigues <developer.davi@gmail.com>
2020-02-12 18:03:02 -0300

    Changed product fields, added seller and limit

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,9 +1,14 @@
 const mercadoLibre = require('mercadolibre')
+const axios = require('axios')
 
 const { MELI_APP, MELI_SECRET } = process.env
 
+const SELF = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://meli-crawler.devdavi.com.br'
+
 export default async function Products(request, response) {
   const { body } = request
+
+  const limit = body.limit || 20
 
   // CACHING
   response.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
@@ -11,19 +16,48 @@ export default async function Products(request, response) {
   try {
     const meli = new mercadoLibre.Meli(MELI_APP, MELI_SECRET)
   
-    meli.get(`/sites/MLB/search?q=${body.search}`, null, (err, res) => {
+    return meli.get(`/sites/MLB/search?q=${body.search}`, null, async (err, res) => {
+
+      if (err)
+        throw Error(err)
+
+      const raw_products = res.results
+      const products = []
+
+      for (let i = 0; i < raw_products.length; i++) {
+
+        if (i == limit) break
+
+        try {
+          const raw_product = raw_products[i]
+          const seller_id = raw_product.seller.id
+          const seller = await axios.get(`${SELF}/api/sellers/${seller_id}`)
+          const seller_name = seller.data ? seller.data.nickname : null
+          
+          const product = {
+            name: raw_product.title,
+            link: raw_product.permalink,
+            price: raw_product.price,
+            store: seller_name,
+            state: raw_product.address.state_name
+          }
+
+          products.push(product)
+        } catch (error) {
+          response.status(500)
+          response.json({ error })
+        }
+      }
       
       response.status(200)
 
       // SEND PRODUCTS RESPONSE
-      response.json(res.results)
+      response.json(products)
     })
 
   } catch (error) {
 
     response.status(500)
-    response.json(error)
+    return response.json(error)
   }
-
-
 }
